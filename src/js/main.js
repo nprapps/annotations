e3d10e619192355ab9c55c0f668cc6cf676f7df0
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2021-06-11 10:25:38 -0400

    Cleaning up gently

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ var flags = require("./flags");
 
 var container = $.one("main.speech");
 
-var { DateFormatter, plural } = require('./util.js');
+var { DateFormatter, plural } = require("./util.js");
 
 var updateAnnotations = function (newSpeech, lastModified) {
   // collect annotations that have been seen before
@@ -42,51 +42,56 @@ var refresh = async function () {
 var removeUnpublishedLinks = function () {
   var annos = $(".annotation").map(a => a.id);
   $("main.speech > *:not(.annotation) a").forEach(function (tag, i) {
-    var [_,link] = tag.href.split("#");
+    var [_, link] = tag.href.split("#");
     if (!link || !annos.includes(link)) {
       tag.removeAttribute("href");
     }
   });
 };
 
-const updateOverview = function() {
+var updateOverview = function () {
   // Update both last updated fields.
   var time = DateFormatter(new Date());
-  $('.last-updated').forEach(t => t.innerHTML = `Last Updated: ${time}`);
+  $(".last-updated").forEach(t => (t.innerHTML = `Last Updated: ${time}`));
 
-  var allAnnos = $('.annotation');
-  $.one('.num-annotations').innerHTML = `${allAnnos.length} annotation${plural(allAnnos.length)}`;
-  
-  
-  // TODO: add in current unseen 
-  var numNew = $(".annotation:not([data-seen])").length || 'no';
-  $.one('.update-number').innerHTML = `${numNew} annotation${plural(numNew)}`
-}
+  var numAll = $(".annotation").length;
+  $.one(".num-annotations").innerHTML = `${numAll} annotation${plural(numAll)}`;
 
-var initializePage = (function () {
+  // TODO: add in current unseen
+  var numNew = $(".annotation:not([data-seen])").length || "no";
+  $.one(".update-number").innerHTML = `${numNew} annotation${plural(numNew)}`;
+};
+
+var initializePage = function () {
+  console.log("Hello!");
   updateOverview();
   removeUnpublishedLinks();
   setInterval(refresh, flags.refresh * 1000);
-})();
+};
+
+initializePage();
 
 // activate live features
 document.body.classList.add("javascript");
 
 // set listeners for link<->annotation connection
-document.body.addEventListener("click", function(e) {
+document.body.addEventListener("click", function (e) {
   var link = e.target.closest(`[href^="#annotation"]`);
   if (link) {
     e.preventDefault();
     var annotation = $.one(link.getAttribute("href"));
     annotation.scrollIntoView({ behavior: "smooth", block: "center" });
-    annotation.querySelector(".content").animate([
-      { borderColor: "", background: "" },
-      { borderColor: "#efc637", background: "#f5ebc8" },
-      { borderColor: "", background: "" }
-    ], {
-      fill: "both",
-      duration: 1000
-    });
+    annotation.querySelector(".content").animate(
+      [
+        { borderColor: "", background: "" },
+        { borderColor: "#efc637", background: "#f5ebc8" },
+        { borderColor: "", background: "" },
+      ],
+      {
+        fill: "both",
+        duration: 1000,
+      }
+    );
     return;
   }
 });
